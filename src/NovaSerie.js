5c265a8af67ffdc70a7a9c3ab6ef27a2f4d08445
sidglas <sidglas@hotmail.com>
2019-10-18 12:36:31 -0300

    adição do gênero na inclusão de série

--- src/NovaSerie.js
+++ src/NovaSerie.js
@@ -1,4 +1,4 @@
-import React , { useState } from 'react'
+import React , { useState , useEffect} from 'react'
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
 
@@ -8,24 +8,70 @@ import { Redirect } from 'react-router-dom'
 const NovaSerie = () => {
   const[name, setName] = useState('')
   const[success, setSuccess] = useState(false)
+  const[form, setForm] = useState([])
+  const[genres, setGenres] = useState([])
+
+  useEffect(() => {
+
+    axios
+    .get('/api/genres')
+    .then(res =>{
+        setGenres(res.data.data)
+    })  
+  }, [])
 
 
   const onChange = evt => {
     setName(evt.target.value)
   }
 
+
+  const onChange1 = field => evt => {
+
+    console.log('CAMPO CAMPO', field)
+
+    if (field !== 'genre_id') {
+
+      console.log('Realmente nao estou em Gênero...')
+      setForm({
+        ...form,
+        [field]: evt.target.value })
+
+    } else {
+
+      let elem = document.getElementById('genero')
+      let item = elem.options[elem.selectedIndex]
+
+      console.log(item, 'AA')
+      console.log(elem.options[elem.selectedIndex].text, 'BB')
+      console.log(item.text, 'CC')
+
+      setForm({
+        ...form,
+        [field]: evt.target.value , 
+        genre: item.text
+      })
+
+
+
+      console.log('form form form', form)
+    }
+  }
+
   
   const save = () => {
 
+      console.log('Vamos ver  form ', {form})
+      let elem = document.getElementById('genero')
+      let item = elem.options[elem.selectedIndex]
+
   axios
-    .post('/api/series', { 
-      name: name,
-     })
+    .post('/api/series', form)
     .then(res => { 
       setSuccess(true) 
     })
+
   }
-  
   if (success) {
      return <Redirect to='/series' />
   }
@@ -38,8 +84,15 @@ const NovaSerie = () => {
       <form>
         <div className='form-group'>
           <label htmlFor='name'>Nome</label>
-          <input type='text' value={name} onChange={onChange} className='form-control' 
+          <input type='text' value={form.name} onChange={onChange1('name')} className='form-control' 
             id='name' placeholder='Nome da Série' />  
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='name'>Gênero</label>
+          <select className='form-control' id="genero" onChange={onChange1('genre_id')} value={form.genre_id}>
+            { genres.map(genre => <option key={genre.id} value={genre.id} selected={genre.id === form.genre}>{genre.name}</option>) }
+          </select>
         </div>
 
         <button type='button' onClick={save} className='btn btn-primary'>Salvar</button>
